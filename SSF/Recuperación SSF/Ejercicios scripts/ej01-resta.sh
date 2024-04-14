b062bf477864814cadd81114f264fd69ef3fe649
Angel <145053589+Angel170605@users.noreply.github.com>
2024-04-14 10:44:10 +0100

    Create ej01-resta.sh

---
+++ SSF/Recuperación SSF/Ejercicios scripts/ej01-resta.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+########################################
+#
+# Nombre: Resta
+# Autor: Ángel Pérez <angelpd170605@gmail.com>
+# Objetivo del script: Restar dos números.
+# Entradas: num1, num2
+# Salidas: result
+#
+########################################
+
+if [ "$# -ne 2" ]; then
+    echo "Error. Debe indicar dos valores"
+    exit
+fi
+
+num1=$1
+num2=$2
+result=$(($num1-$num2))
+
+echo "El resultado de la operación $num1 - $num2 = $result"
