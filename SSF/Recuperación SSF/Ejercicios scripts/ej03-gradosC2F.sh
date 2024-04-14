3d3825f890ae50c9573e1e54ef2e027b196052ac
Angel <145053589+Angel170605@users.noreply.github.com>
2024-04-14 11:01:59 +0100

    Create ej03-gradosC2F.sh

---
+++ SSF/Recuperación SSF/Ejercicios scripts/ej03-gradosC2F.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+########################################
+#
+# Nombre: Grados C2F
+# Autor: Ángel Pérez <angelpd170605@gmail.com>
+# Objetivo del script: Convierte el argumento de entrada (c: Temperatura en grados Celsius) a grados Fahrenheit con el uso de la fórmula " f = c * 1.8".
+# Entradas: c (Temperatura en grados Celsius)
+# Salidas: f (c convertida en grados Farenheit)
+#
+########################################
+
+if [ $# -ne 1 ]; then
+    echo "Error. Debe indicar una temperatura en grados Celsius."
+    exit
+fi
+
+c=$1
+
+f=$(echo "$c * 1.8 + 32" | bc)
+
+echo "$c grados Celsius equivalen a $f grados Farenheit"
+
